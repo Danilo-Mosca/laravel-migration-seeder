12d61a7443ceb4d83b04eff00c9f4a38beebf52d
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-03-31 20:13:44 +0200

    model created and added to the search for trains departing from today's date onwards

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     public function index(){
-        // $trains =...;
-        return view("index");
+        // $trains = Train::all();
+        // dd($trains);
+
+        // Restituisco tutti i treni che sono in partenza dalla data odierna in avanti, in ordine cronologico:
+        $trains = Train::orderBy('data_partenza', "ASC")->where('data_partenza', '>=', date("Y-m-d"))->get();
+        // dd($trains);
+        return view("index", compact('trains'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,5 +12,20 @@
 
 {{-- Contenuto personalizzato per ogni pagina: --}}
 @section('content')
-    <h2>Homepage</h2>
+    <ol>
+        @foreach ($trains as $train)
+            <li>
+                {{ $train->azienda }}
+                {{ $train->stazione_di_partenza }}
+                {{ $train->stazione_di_arrivo }}
+                {{ $train->orario_di_partenza }}
+                {{ $train->orario_di_arrivo }}
+                {{ $train->codice_treno }}
+                {{ $train->totale_carrozze }}
+                {{ $train->in_orario }}
+                {{ $train->cancellato }}
+                {{ $train->data_partenza }}
+            </li>
+        @endforeach
+    </ol>
 @endsection
